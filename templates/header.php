ca81ec51ddbbaf167188d6fb23e2c692b7b70889
Mohamad Inalo <88008468+Mo-Inalo@users.noreply.github.com>
2023-05-25 13:20:26 +0200

    navbar angepasst

--- templates/header.php
+++ templates/header.php
@@ -8,7 +8,7 @@ $page = basename($_SERVER['PHP_SELF'],'.php');
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Raman e.V. | <?php echo $page == 'index'? $page='Homepage':$page ?></title>
-    <link rel="icon" type="image/x-icon" href="/images/logo3.png">
+    <link rel="icon" type="image/x-icon" href="/images/ramanlogo.png">
 
     <!-- css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
