85a93cf6be1df2636728211d6ccb0576d8668a02
Mo-Inalo <Mimoin@92>
2024-02-20 17:15:19 +0100

    rebiulding

--- Satzung.php
+++ Satzung.php
@@ -8,52 +8,62 @@ include "templates/nav.php"
 ?>
 
 
-<main style="max-width: 900px; margin-right: auto; margin-left: auto">
 
 
-<div class="py-md-5" style=" position: relative;margin-left: auto; margin-right: auto; width: 90%; min-height: 150vh;  top: 20px">
-    <center>
-        <h1 style="font-family: Calibri,Andalus"> Satzung - RAMAN e.V. </h1>
-        <p style="text-align: justify">
-            Herzlich willkommen zur Satzung unseres Vereins!
+    <div class="container" style="max-width: 900px; margin-right: auto; margin-left: auto">
+        <div class="row-md-2">
+            <div class="col p-5">
+                <center>
+                    <h1 style="font-family: Calibri,Andalus"> Satzung - RAMAN e.V. </h1>
+                    <p style="text-align: justify">
+                        Herzlich willkommen zur Satzung unseres Vereins!
 
-            Wir freuen uns, dass Sie Interesse daran haben, mehr über die Grundlagen und Richtlinien unseres Vereins zu erfahren.
-            Die Satzung bildet das rechtliche Fundament unserer Organisation und legt die Rahmenbedingungen fest, nach denen wir handeln und agieren.
+                        Wir freuen uns, dass Sie Interesse daran haben, mehr über die Grundlagen und Richtlinien unseres Vereins zu erfahren.
+                        Die Satzung bildet das rechtliche Fundament unserer Organisation und legt die Rahmenbedingungen fest, nach denen wir handeln und agieren.
 
-            Unser Verein wurde gegründet, um [Zweck oder Mission des Vereins kurz beschreiben, z. B. Förderung des Sports, der Kultur,
-            des sozialen Engagements usw.]. In dieser Satzung finden Sie alle wichtigen Informationen darüber, wie unser Verein
-            organisiert ist, welche Rechte und Pflichten unsere Mitglieder haben und welche Ziele wir verfolgen.
+                          In dieser Satzung finden Sie alle wichtigen Informationen darüber, wie unser Verein
+                        organisiert ist, welche Rechte und Pflichten unsere Mitglieder haben und welche Ziele wir verfolgen.
 
-            Es ist uns wichtig, transparent und offen über unsere Strukturen und Prozesse zu informieren. Die Satzung dient
-            als Leitfaden für unsere Aktivitäten und stellt sicher, dass wir im Einklang mit unseren Werten und Zielen handeln.
+                        Es ist uns wichtig, transparent und offen über unsere Strukturen und Prozesse zu informieren. Die Satzung dient
+                        als Leitfaden für unsere Aktivitäten und stellt sicher, dass wir im Einklang mit unseren Werten und Zielen handeln.
 
-            Wir laden Sie herzlich ein, die Satzung unseres Vereins aufmerksam zu lesen und bei Fragen oder Anmerkungen
-            gerne auf uns zuzukommen. Ihr Verständnis für unsere rechtlichen Grundlagen ist uns wichtig, um gemeinsam
-            eine positive und erfolgreiche Zukunft für unseren Verein zu gestalten.
+                        Wir laden Sie herzlich ein, die Satzung unseres Vereins aufmerksam zu lesen und bei Fragen oder Anmerkungen
+                        gerne auf uns zuzukommen. Ihr Verständnis für unsere rechtlichen Grundlagen ist uns wichtig, um gemeinsam
+                        eine positive und erfolgreiche Zukunft für unseren Verein zu gestalten.
 
-            Vielen Dank für Ihr Interesse und Ihre Unterstützung!
-        </p>
+                        Vielen Dank für Ihr Interesse und Ihre Unterstützung!
+                        Ihr RAMAN Team!
+                    </p>
+                <br>
+                    <a href="/pdf/Satzung_RAMAN_e._V..pdf " download="Satzung-RAMAN e.V.s.pdf">
+                        <button class="btn btn-success btn-lg btn-block">Satzung Herunterladen</button>
+                    </a>
 
-            <a href="/pdf/Satzung_RAMAN_e._V..pdf " download="Satzung-RAMAN e.V.s.pdf">
-                <button class="btn btn-success btn-lg btn-block">Satzung runterladen</button>
-            </a>
+
+                </center>
+            </div>
+            <div class="col">
 
 
-    </center>
-    <br>
-<center>
-    <iframe src="/pdf/Satzung_RAMAN_e._V..pdf" style="width: 90% ; min-height: 70vh"></iframe>
-</center>
+
+                <center>
+                    <iframe src="/pdf/Satzung_RAMAN_e._V..pdf" style="width: 90% ; min-height: 70vh"></iframe>
+                </center>
+            </div>
+            <br>
+            <br>
+        </div>
+
 
 
 </div>
 
 
-</main>
 
-<div style="top: 20px">
+
+
     <?php
     include "templates/footer.php"
     ?>
-</div>
+
 
