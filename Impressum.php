efc5d8924c6627720d7069216cfeb9b49487497f
Mo-Inalo <Mimoin@92>
2024-02-24 00:00:47 +0100

    rebiulding

---
+++ Impressum.php
@@ -0,0 +1,101 @@
+
+
+<?php
+include "templates/header.php"
+?>
+
+
+<?php
+include "templates/nav.php"
+?>
+
+
+<div class="containercontact" style=" ">
+
+
+
+    <div class=" p-3" style=" max-height:90%; max-width: 1200px ; margin-left: auto; margin-right: auto;">
+
+
+
+        <div class="row" style=" max-width: 99vw">
+
+            <div class="col-md mainfont" style="max-height: 100%; width: 100%">
+
+                <h1>Impressum</h1>
+                <p>Angaben gemäß § 5 TMG:</p>
+
+                <h4> RAMAN e.V.</h4>
+                <h5> Amtsgericht Bonn</h5>
+                <h5> Bonnerstr 123</h5>
+                <h5> 53111 Bonn</h5>
+
+
+                <h5 style="display: inline-block; font-weight: bolder"> E-Mail:</h5>
+                <h5 style="display: inline-block">info@raman.world</h5>
+                <br>
+                <h5 style="display: inline-block; font-weight: bolder"> Telefone</h5>
+                <h5 style="display: inline-block">0176xxxxxxx</h5>
+                <br>
+
+
+                Telefon: 01234/567890
+
+                Fax: 0123/4567890
+
+                E-Mail: info@raman-verein.de
+
+                Vertreten durch:
+
+                Vorsitzende:
+                Nina Mustermann
+
+                c/o Musterstraße 1
+
+                12345 Musterstadt
+
+                Telefon: 01234/567890-1
+
+                E-Mail: nina.mustermann@raman-verein.de
+
+                Vorsitzender:
+                Max Beispiel
+
+                c/o Musterstraße 1
+
+                12345 Musterstadt
+
+                Telefon: 01234/567890-2
+
+                E-Mail: max.beispiel@raman-verein.de
+
+                Registergericht: Amtsgericht Musterstadt
+
+                Registernummer: VR 12345
+
+                Umsatzsteuer-Identifikationsnummer gemäß § 27a UStG: DE 123456789
+
+                Inhaltlich Verantwortlicher gemäß § 55 Abs. 2 RStV:
+
+                Nina Mustermann
+
+                c/o Musterstraße 1
+
+                12345 Musterstadt
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+
+</div>
+
+
+
+<?php
+include "templates/footer.php"
+?>
