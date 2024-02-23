641a56ee4d7572947b010b6081d5247a8841eb6b
Mo-Inalo <Mimoin@92>
2024-02-23 01:46:39 +0100

    rebiulding

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,7 @@ $page = basename($_SERVER['PHP_SELF'],'.php');
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>RAMAN | <?php echo $page == 'index'? $page='Homepage':$page ?></title>
+    <title>RAMAN | <?php echo $page == 'index'? $page='Startseite':$page ?></title>
     <link rel="icon" type="image/x-icon" href="/images/ramanlogo.png">
 
     <!-- css -->
